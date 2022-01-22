2bf1a703c95b4320301149c4fc0d9144f109b4dd
saman <s.saman@iitg.ac.in>
2022-01-23 05:29:44 +0530

    static

--- ensure/settings.py
+++ ensure/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY',default="1234vgrre3")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
